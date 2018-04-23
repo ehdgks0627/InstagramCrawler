690c8bb06edc33ae8eee321020160249886d0609
ehdgks0627 <ehdgks0627@naver.com>
2018-04-23 12:21:03 +0900

    Added compare & save db

--- HashTagSearchManager.py
+++ HashTagSearchManager.py
@@ -1,45 +1,165 @@
-from HashTagSearch import HashTagSearch
 import sqlite3
+import os
+import cv2
+from HashTagSearch import HashTagSearch
 
 
 class HashTagSearchManager(HashTagSearch):
-    def __init__(self):
+    def __init__(self, storage_folder='images'):
         super().__init__()
-        self.total_posts = 0
+        self.storage_path = os.path.join(os.getcwd(), storage_folder)
+        if not os.path.exists(self.storage_path):
+            os.mkdir(self.storage_path)
+
+        # initialize DB
         self.conn = sqlite3.connect("database.sqlite3")
         self.cur = self.conn.cursor()
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS Image (
-                            path VARCHAR(256),
-                            tag VARCHAR(256),
-                            post_id VARCHAR(256))""")
         self.cur.execute("""CREATE TABLE IF NOT EXISTS Post (
-                            post_id VARCHAR(256), 
+                            id VARCHAR(256) PRIMARY KEY, 
                             code VARCHAR(256), 
                             caption VARCHAR(256),
                             user VARCHAR(256), 
                             display_src VARCHAR(256), 
                             is_video VARCHAR(256), 
                             created_at VARCHAR(256))""")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS Image (
+                            path VARCHAR(256),
+                            url VARCHAR(256),
+                            post_id VARCHAR(256) PRIMARY KEY)""")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS SourceImage (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            path VARCHAR(256))""")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS Similarity (
+                            value DOUBLE,
+                            post_id INTEGER,
+                            source_image_id INT,
+                            FOREIGN KEY(post_id) REFERENCES Image(post_id),
+                            FOREIGN KEY(source_image_id) REFERENCES SourceImage(id))""")
         self.conn.commit()
+        self.total_posts = len(self.cur.execute("SELECT * FROM Post").fetchall())
+
+        self.source_image_insert_sql = "INSERT INTO SourceImage(path) VALUES(?)"
+        self.post_insert_sql = "INSERT INTO Post(id, code, caption, user, display_src, is_video, created_at) VALUES(?, ?, ?, ?, ?, ?, ?)"
+        self.image_insert_sql = "INSERT INTO Image(path, url, post_id) VALUES(?, ?, ?)"
+        self.similarity_insert_sql = "INSERT INTO Similarity(value, post_id, source_image_id) VALUES(?, ?, ?)"
 
     def __del__(self):
+        self.cur.close()
         self.conn.close()
+
+    def download_image(self, url, path):
+        """
+        :param url: image url to download
+        :param path: path to save image
+        :return: true when download success. if on error, false
+        """
+        try:
+            with open(path, 'wb') as f:
+                f.write(self.session.get(url).content)
+                return True
+        except Exception as e:
+            print("download_image Exception: " + e)
+            return False
+
+    def compare_image(self, path1, path2, similarity_threshold=0.8):
+        try:
+            img1 = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
+            img2 = cv2.imread(path2, cv2.IMREAD_GRAYSCALE)
+
+            if img1 is None or img2 is None:
+                return None
+
+            if img1.shape[1] < img2.shape[1]:
+                img2 = img2[:,
+                       int((img2.shape[1] - img1.shape[1]) / 2):int(
+                           img2.shape[1] + (img2.shape[1] - img1.shape[1]) / 2)]
+            elif img2.shape[1] > img1.shape[1]:
+                img1 = img1[:,
+                       int((img1.shape[1] - img2.shape[1]) / 2):int(
+                           img1.shape[1] + (img2.shape[1] - img1.shape[1]) / 2)]
+
+            orb = cv2.ORB_create()
+
+            kp1, des1 = orb.detectAndCompute(img1, None)
+            kp2, des2 = orb.detectAndCompute(img2, None)
+
+            # create BFMatcher object
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+            # Match descriptors.
+            matches = bf.match(des1, des2)
+
+            similarity = (len(matches) * 2) / (len(kp1) + len(kp2))
+            if similarity > similarity_threshold:
+                print("Match!")
+
+            """
+            # Sort them in the order of their distance.
+            matches = sorted(matches, key=lambda x: x.distance)
+
+            # Draw first 10 matches.
+            img3 = None
+            img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, flags=2, outImg=img3)
+
+            plt.imshow(img3)
+            plt.show()
+            """
+            return similarity
+
+        except:
+            return None
 
     def save_results(self, instagram_results):
         super().save_results(instagram_results)
-        sql = "INSERT INTO Post VALUES (?, ?, ?, ?, ?, ?, ?)"
-        datas = []
 
-        for i, post in enumerate(instagram_results):
-            datas.append((str(post.post_id),
-                          str(post.code),
-                          str(post.caption),
-                          str(post.user),
-                          str(post.display_src),
-                          str(post.is_video),
-                          str(post.created_at)))
+        posts = []
+        self.cur.execute("SELECT id FROM Post")
+        id_set = set(map(lambda x: x[0], self.cur.fetchall()))
 
-        self.cur.executemany(sql, datas)
-        self.total_posts += len(datas)
-        print("[Total Post - %d] %d - Post Inserted" % (self.total_posts, len(datas)))
-        # TODO(sprout): save image
+        for post in instagram_results:
+            if post.post_id not in id_set:
+                id_set.add(post.post_id)
+                posts.append((str(post.post_id),
+                              str(post.code),
+                              str(post.caption),
+                              str(post.user.id),
+                              str(post.display_src),
+                              str(post.is_video),
+                              str(post.created_at)))
+        self.cur.executemany(self.post_insert_sql, posts)
+        self.conn.commit()
+        self.total_posts += len(posts)
+        print("[Total Post - %d] %d - Post Inserted" % (self.total_posts, len(posts)))
+
+        self.cur.execute("SELECT post_id FROM Image")
+        image_set = set(map(lambda x: x[0], self.cur.fetchall()))
+        images = []
+        for post in posts:
+            if post[0] not in image_set:
+                image_set.add(post[0])
+                url = post[4]  # display_src
+                path = os.path.join(self.storage_path, url.split('/')[-1])
+                if self.download_image(url, path):
+                    images.append((path, url, post[0]))
+        self.cur.executemany(self.image_insert_sql, images)
+        self.conn.commit()
+        print("%d - Image Downloaded" % (len(images)))
+
+        for source_image in self.cur.execute("SELECT id, path FROM SourceImage").fetchall():
+            source_image_id = source_image[0]
+            source_image_path = source_image[1]
+            print("Compare with %s" % (source_image_path))
+
+            self.cur.execute(
+                "SELECT post_id, path FROM Image WHERE post_id NOT IN (SELECT post_id FROM Similarity WHERE source_image_id=?)",
+                (source_image_id,))
+            similaritys = []
+            for image in self.cur.fetchall():
+                image_post_id = image[0]
+                image_path = image[1]
+
+                similarity = self.compare_image(source_image_path, image_path)
+                if similarity is not None:
+                    similaritys.append((similarity, image_post_id, source_image_id))
+            self.cur.executemany(self.similarity_insert_sql, similaritys)
+            self.conn.commit()
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from HashTagSearchManager import HashTagSearchManager
 import logging as log
+import os
 
 if __name__ == '__main__':
     log.basicConfig(level=log.INFO)
-    HashTagSearchManager().extract_recent_tag("food")
+    manager = HashTagSearchManager()
+    while True:
+        print("0. 사용법 출력")
+        print("1. 일반 모드")
+        print("2. 비교할 이미지 등록하기")
+        print("3. 결과 보기")
+        choice = int(input(">>> "))
+        if choice == 0:
+            print("일반 모드 - [키워드입력 -> 이미지 크롤링 -> 등록된 이미지와 비교 반복]")
+            print("비교할 이미지 등록하기 - [비교할 이미지를 사전에 등록합니다]")
+            print("결과보기 - 비교 결과를 봅니다")
+        elif choice == 1:
+            print("검색할 키워드를 입력하세요")
+            keyword = input(">>> ")
+            manager.extract_recent_tag(keyword)
+        elif choice == 2:
+            print("경로를 입력하세요")
+            path = input(">>> ")
+            if not os.path.exists(path):
+                print("파일을 찾을 수 없습니다")
+            else:
+                manager.cur.execute(manager.source_image_insert_sql, (path,))
+                manager.conn.commit()
+                print("정상적으로 등록되었습니다")
+        elif choice == 3:
+            pass  # TODO(sprout): ...
